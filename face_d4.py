9165d62bc5ad9fb09e7a98e57c33661b8c708161
yokochan20 <30590684+yokochan20@users.noreply.github.com>
2017-08-01 19:05:09 +0900

    Update face_d4.py

--- face_d4.py
+++ face_d4.py
@@ -5,7 +5,7 @@ import sys
 import os
 import shutil
 import random
-#HAAR分類器の顔検出用の特徴量
+#HAAR分類器顔検出用の特徴量
 #cascade_path = "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
 #cascade_path = "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
 cascade_path = "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml"
@@ -28,15 +28,10 @@ for x in range(150):
 #カスケード分類器の特徴量を取得する
 	cascade = cv2.CascadeClassifier(cascade_path)
 
-#物体認識（顔認識）の実行
-#image – CV_8U 型の行列．ここに格納されている画像中から物体が検出されます
+#顔認識の実行
 #objects – 矩形を要素とするベクトル．それぞれの矩形は，検出した物体を含みます
-#scaleFactor – 各画像スケールにおける縮小量を表します
-#minNeighbors – 物体候補となる矩形は，最低でもこの数だけの近傍矩形を含む必要があります
-#flags – このパラメータは，新しいカスケードでは利用されません．古いカスケードに対しては，cvHaarDetectObjects 関数の場合と同じ意味を持ちます
-#minSize – 物体が取り得る最小サイズ．これよりも小さい物体は無視されます
+#minSize – 物体が取り得る最小サイズ
 	facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-#facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=3, minSize=(10, 10), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
 
 	print ("face rectangle")
 	print (facerect)
@@ -63,15 +58,3 @@ for x in range(150):
 		count =count - 1
 		c = c + 1
 
-
-'''
-	if len(facerect) > 0:
-   
-    #検出した顔を囲む矩形の作成
-		for rect in facerect:
-			cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
-
-    #認識結果の保存
-		cv2.imwrite('/Users/SatoshiYokoyama/D-HACKS/face_dataset/face_1d/de1_'+str(count)+'.JPG' ,image)
-	count += 1
-'''
