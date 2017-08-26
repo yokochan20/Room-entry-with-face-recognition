a6d8f3dea2216571727d4544b935858272677647
yokochan20 <satooshi20sl@gmail.comp>
2017-08-26 15:47:06 +0900

    recognition.pyをPEP8に

--- recognition.py
+++ recognition.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import cv2
 import time
+
 from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
 from unlock_client import callraspi
